9ba67d562b2a760f4a14a88193808f34e4422ec0
Juliano <juliano.sobrinhojunior@gamil.com>
2025-12-30 12:24:01 -0300

    alterações no RDE já feitas e funcional, no entanto problemas de endereçamento no netlify

--- controller/rde-com.js
+++ controller/rde-com.js
@@ -122,9 +122,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const opt = {
       margin: [5, 5, 5, 5],
       filename: `${dados.local + dados.tag}.pdf`,
-      image: { type: "jpeg", quality: 0.98 },
-      html2canvas: { scale: 2, useCORS: true },
-      jsPDF: { unit: "mm", format: "a4", orientation: "portrait" }
+      image: { type: "jpeg", quality: 0.98 }
     };
 
     html2pdf().set(opt).from(element).save();
